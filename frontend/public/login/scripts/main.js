5dea0d08db244773d29bba44311c6e070129883c
Auresol <phudis27@gmail.com>
2024-04-24 04:13:32 +0700

    integrade front

--- frontend/public/login/scripts/main.js
+++ frontend/public/login/scripts/main.js
@@ -1,12 +1,38 @@
+
 import { handleLogin } from './api.js';
 document.addEventListener('DOMContentLoaded', function() {
   const submitButton = document.getElementById('submit-button');
   submitButton.addEventListener('click', function() {
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+
+    if (email.trim() === '' || password.trim() === '') {
+          alert('Please fill in all fields');
+          return;
+    }
+
     console.log("enter event listener")
-    console.log('email', email.value);
+    console.log('email', email);
     
-    handleLogin(email.value, password.value);
+    handleLogin(email, password);
   });
 });
+
+// import { handleLogin } from './api.js';
+
+// function submitForm() {
+//   var email = document.getElementById('email').value;
+//   var password = document.getElementById('pass').value;
+
+//   if (email.trim() === '' || password.trim() === '') {
+//     alert('Please fill in all fields');
+//     return;
+//   }
+
+//   console.log('Email:', email);
+//   console.log('Password:', password);
+
+//   handleLogin(email, password);
+// }
+
+// export { submitForm };
--- frontend/server.js
+++ frontend/server.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
