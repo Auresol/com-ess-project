5cdbaba89c6599f8cec897a29d2c686258eb817d
Auresol <phudis27@gmail.com>
2024-04-25 15:28:54 +0700

    final commit

--- frontend/server.js
+++ frontend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 
 app.use(express.static('public'));
+app.use('/images', express.static('images'));
 
 const PORT = process.env.PORT || 4000;
 
